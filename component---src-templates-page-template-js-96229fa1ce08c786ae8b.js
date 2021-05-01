d66a855f3b0fc6ab6dac6c7797d0d19f7d78eddc
Brian St John <stjohn.b@gmail.com>
2021-05-01 17:50:42 -0500

    Updates

---
+++ component---src-templates-page-template-js-96229fa1ce08c786ae8b.js
@@ -0,0 +1,2 @@
+(self.webpackChunkbonneville=self.webpackChunkbonneville||[]).push([[395],{2137:function(e,t,n){"use strict";n.r(t);var l=n(7294),a=n(4983),c=n(2335);t.default=function(e){var t=e.pageContext.page;return l.createElement(l.Fragment,null,l.createElement(c.Z,{title:t.name,description:t.title}),l.createElement("div",{className:"page-standard"},l.createElement(a.Z,{content:t.title}),l.createElement("p",null,t.content)))}}}]);
+//# sourceMappingURL=component---src-templates-page-template-js-96229fa1ce08c786ae8b.js.map
